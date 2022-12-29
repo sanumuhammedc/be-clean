cb104692e850d3abbbc24ca2687d2a2a16970d32
sanumuhammedc <sanumuhammed.c@gmail.com>
2022-12-29 13:00:15 +0530

    initial set up

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Inter } from '@next/font/google'
+import { Alert } from "flowbite-react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      
+    </>
+  )
+}
